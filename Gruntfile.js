59f7c7fd3122c35e230172a830f82064f91ad088
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2013-06-28 22:39:08 +0800

    Remove grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,21 +28,6 @@ module.exports = function (grunt) {
         "test/coverage/**/*"
       ]
     },
-    concat: {
-      backgrid: {
-        options: {
-          banner: '/*!\n  <%= pkg.name %>\n' +
-            '  <%= pkg.repository.url %>\n\n' +
-            '  Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            '  Licensed under the MIT license.\n' +
-            '*/\n\n'
-        },
-        src: [
-          "backgrid-text-cell.js"
-        ],
-        dest: "backgrid-text-cell.js"
-      }
-    },
     jasmine: {
       test: {
         version: "1.3.1",
@@ -119,7 +104,6 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-contrib-concat");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-recess");
   grunt.loadNpmTasks("grunt-jsduck");
