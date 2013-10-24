bb1b63641cb3af649b8b46147a0e387f0038ff95
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2013-10-24 23:41:09 +0800

    Prevent template vars from clashing with global vars

--- backgrid-text-cell.js
+++ backgrid-text-cell.js
@@ -35,7 +35,7 @@
     className: "modal hide fade",
 
     /** @property {function(Object, ?Object=): string} template */
-    template: _.template('<form><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h3><%- column.get("label") %></h3></div><div class="modal-body"><textarea cols="<%= cols %>" rows="<%= rows %>"><%- content %></textarea></div><div class="modal-footer"><input class="btn" type="submit" value="Save"/></div></form>'),
+    template: _.template('<form><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h3><%- column.get("label") %></h3></div><div class="modal-body"><textarea cols="<%= cols %>" rows="<%= rows %>"><%- content %></textarea></div><div class="modal-footer"><input class="btn" type="submit" value="Save"/></div></form>', null, {variable: null}),
 
     /** @property */
     cols: 80,
